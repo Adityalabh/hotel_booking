1a4f5202e50f73fa533c1b99f5931e4c04f3b324
Aditya Labh <adityakumarlabh12@gmail.com>
2024-10-06 20:07:39 +0530

    second commit

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = () => {
   return (
     //here outlet is used for nested routing which serves as a placeholder for rendering matched child routes.
     <div className="flex flex-col relative overflow-x-hidden min-h-screen  ">
-      <div className="px-9 fixed w-[100vw] z-10 px-3 shadow-sm shadow-red-700 bg-white">
+      <div className=" fixed w-[100vw] z-10 px-3 shadow-sm shadow-red-700 bg-white">
         <HeaderPage />
       </div>
       <div className="  pt-[100px] bg-gray-200 w-full">
