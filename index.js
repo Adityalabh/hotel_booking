b4c6ba27f7f367c92e00c970d070865e020da6fa
Aditya Labh <adityakumarlabh12@gmail.com>
2024-10-09 11:15:28 +0530

    updating server

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const cors = require('cors');//it helps api server to communicate and share reso
 const mongoose = require('mongoose');//to have connectivity with mongodb 
 const bcrypt = require('bcryptjs');//to make password more secure instead of passing password as simple text
 const jwt = require('jsonwebtoken');//this is 
-const User = require('./models/User');//to access user schema
+const User = require('./api/models/User');//to access user schema
 const cookieParser = require('cookie-parser');//to parse the cookie
 const imageDownloader = require("image-downloader");
 const multer = require('multer');
 const fs = require('fs');//this helps in renaming file name
-const Place = require('./models/Place');
-const Booking = require("./models/booking");
+const Place = require('./api/models/Place');
+const Booking = require("./api/models/booking");
 const { rejects } = require('assert');
 
-require('dotenv').config({ path: '../.env' });//to handle .env file to hold important info like mongodb password
+require('dotenv').config();//to handle .env file to hold important info like mongodb password
 const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);//to add more salt to password
