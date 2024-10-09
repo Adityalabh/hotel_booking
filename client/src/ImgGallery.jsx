6f6b66786f64ee87beaaf385732aaecceb112564
Aditya Labh <adityakumarlabh12@gmail.com>
2024-10-09 13:10:04 +0530

    updating server

--- client/src/ImgGallery.jsx
+++ client/src/ImgGallery.jsx
@@ -23,7 +23,7 @@ const ImgGallery = ({placeData}) => {
 
         {placeData.photos?.[0] && (
           <img
-            src={`http://localhost:4000/uploads/${placeData.photos[0]}`}
+            src={`https://hotel-booking-5-kpao.onrender.com/uploads/${placeData.photos[0]}`}
             alt=""
             className="object-cover cursor-pointer"
             onClick={() => setShowAllPhoto(true)}
@@ -33,7 +33,7 @@ const ImgGallery = ({placeData}) => {
       <div className="grid  gap-2 ">
         {placeData.photos?.[1] && (
           <img
-            src={`http://localhost:4000/uploads/${placeData.photos[1]}`}
+            src={`https://hotel-booking-5-kpao.onrender.com/uploads/${placeData.photos[1]}`}
             alt=""
             className="aspect-square  object-cover cursor-pointer"
             onClick={() => setShowAllPhoto(true)}
@@ -43,7 +43,7 @@ const ImgGallery = ({placeData}) => {
         <div className="">
           {placeData.photos?.[2] && (
             <img
-              src={`http://localhost:4000/uploads/${placeData.photos[2]}`}
+              src={`https://hotel-booking-5-kpao.onrender.com/uploads/${placeData.photos[2]}`}
               alt=""
               className="aspect-square  object-cover cursor-pointer"
               onClick={() => setShowAllPhoto(true)}
--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use('/uploads', express.static(__dirname + '/uploads'));
 
 app.use(cors({
     credentials: true,
-    origin: 'http://localhost:5173',
+    origin: process.env.URL,
 }));
 
 // console.log(process.env.MONGO_URL);
